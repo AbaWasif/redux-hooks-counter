55f379236b09e3bbbeb42a15ce38cabb8bad2a4f
AbaWasif <abanoub.wasif@hotmail.com>
2019-07-04 04:10:24 +1000

    Implemented Lifecycle Hook 'useEffect'

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
-import { useSelector, useDispatch } from "react-redux";
+import Counter from "./components/counter";
 
 function App() {
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
+  
+
   return (
     <div className="App">
       <header className="App-header wrapper">
-        <div className="row">
-          <h1 className="w-100">Counter : {counter} </h1>
-          <br />
-          <br />
-          <button
-            className="col-sm btn-primary btn-sm"
-            onClick={() => dispatch({ type: "INCREMENT" })}
-          >
-            Increase
-          </button>
-
-          <button
-            className="col-sm btn-danger btn-sm"
-            onClick={() => dispatch({ type: "DECREMENT" })}
-          >
-            Decrease
-          </button>
-        </div>
+        <Counter />
       </header>
     </div>
   );
---
+++ src/components/counter.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("This is my Counter App");
+  });
+
+  return (
+    <div className="row">
+      <h1 className="w-100">Counter : {counter} </h1>
+      <br />
+      <br />
+      <button
+        className="col-sm btn-primary btn-sm"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        Increase
+      </button>
+
+      <button
+        className="col-sm btn-danger btn-sm"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
