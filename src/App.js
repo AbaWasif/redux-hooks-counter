8c34976a3ff86ef6fd08d305314e2ea2f37ec4ee
AbaWasif <abanoub.wasif@hotmail.com>
2019-07-04 02:58:38 +1000

    Redux and hooks inplemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header wrapper">
+        <div className="row">
+          <h1 className="w-100">Counter : {counter} </h1>
+          <br />
+          <br />
+          <button
+            className="col-sm btn-primary btn-sm"
+            onClick={() => dispatch({ type: "INCREMENT" })}
+          >
+            Increase
+          </button>
+
+          <button
+            className="col-sm btn-danger btn-sm"
+            onClick={() => dispatch({ type: "DECREMENT" })}
+          >
+            Decrease
+          </button>
+        </div>
       </header>
     </div>
   );
